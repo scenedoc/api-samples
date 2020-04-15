f8df887edf581ceb6a199ecee8cb86a01e7868dc
Andres Villamarin <andresjoelv@gmail.com>
2020-04-15 11:08:52 -0400

    Bug fixes - Date - Error handling - Timeout

--- powershell/reporting/generate-report.ps1
+++ powershell/reporting/generate-report.ps1
@@ -1,14 +1,22 @@
-$baseUrl = "https://qa.scenedoc.com"
+$baseUrl = ""
 
-$accessKeyId = "SDIDEIMTSVCMFLRIC3GEXCFXE4ECP"
+$accessKeyId = ""
 $token = ""
 
+#### API parameters ####
 # Change as needed
-$formCanonicalId = 18349
-$reportFormat = "~PDF Mapping 2_MAPPING"
-$dateFrom = (Get-Date (Get-Date).AddDays(-7) -UFormat %s) + "000" # will fetch anything in the last 7 days
-$dateTo = (Get-Date -UFormat %s) + "000"
+# ~ - search form mapping by name
+# ^ - search report format by name
+$formCanonicalId = 12345
+$reportFormat = "~PDF Mapping 2_MAPPING,^Sunridge"
 
+$dateFrom = [int][double]::Parse((Get-Date -Date ((get-date).AddDays(-7).ToUniversalTime()) -UFormat %s)) # will fetch anything in the last 7 days
+$dateFromUnix = $dateFrom.ToString() + "000"
+$dateTo = [int](Get-Date -UFormat %s -Millisecond 0)
+$dateToUnix = $dateTo.ToString() + "000"
+
+# modify at own risk
+$Timeout = 30 # seconds
 function Get-BasicAuthCreds {
     param([string]$Username,[string]$Password)
     if([string]::IsNullOrEmpty($Username) -or [string]::IsNullOrEmpty($Password)){
@@ -24,51 +32,66 @@ function Get-BasicAuthCreds {
 $params = @{
     "formCanonicalId"=$formCanonicalId;
     "reportFormat"=$reportFormat;
-    "dateFrom"=$dateFrom;
-    "dateTo"=$dateTo;
+    "dateFrom"=$dateFromUnix;
+    "dateTo"=$dateToUnix;
 }
 
 $BasicCreds = Get-BasicAuthCreds -Username $accessKeyId -Password $token
 if(![string]::IsNullOrEmpty($BasicCreds)){
-    $r = Invoke-WebRequest -Uri $baseUrl"/rest/reporting" -Method Post -Headers @{"Authorization"="Basic $BasicCreds"} -Body ($params|ConvertTo-Json) -ContentType "application/json"
-    $r.StatusCode
-    $content = ConvertFrom-Json $r.Content
+    try {
+        $r = Invoke-WebRequest -Uri $baseUrl"/rest/reporting" -Method Post -Headers @{"Authorization"="Basic $BasicCreds"} -Body ($params|ConvertTo-Json) -ContentType "application/json" -TimeoutSec $Timeout
+        $r.StatusCode
 
-    if ($r.StatusCode -eq "200")
-    {
-        $id = $content.id
-        Write-Output "processing, please wait..."
-        do {
-            $rGetTimelineEntryByIdResponse = Invoke-WebRequest -Uri $baseUrl"/rest/timelines/"$id -Method Get -Headers @{"Authorization"="Basic $BasicCreds"}
-            $contentUploaded = ConvertFrom-Json $rGetTimelineEntryByIdResponse.Content
-        } while($contentUploaded.mediaStatus -ne "UPLOADED" -AND $contentUploaded.entryType -ne "NOTE")
+        if ($r.StatusCode -eq "200")
+        {
+            $content = ConvertFrom-Json $r.Content
+            $id = $content.id
+            Write-Output "processing, please wait..."
+            $contentUploaded = $null
+            do {
+                $rGetTimelineEntryByIdResponse = Invoke-WebRequest -Uri $baseUrl"/rest/timelines/"$id -Method Get -Headers @{"Authorization"="Basic $BasicCreds"} -TimeoutSec $Timeout
+                $contentUploaded = ConvertFrom-Json $rGetTimelineEntryByIdResponse.Content
+            } while($contentUploaded.mediaStatus -ne "UPLOADED")
 
-        $rGetTimelineEntryByIdResponse = Invoke-WebRequest -Uri $baseUrl"/rest/timelines/"$id -Method Get -Headers @{"Authorization"="Basic $BasicCreds"}
-        $contentUploaded = ConvertFrom-Json $rGetTimelineEntryByIdResponse.Content
-        if($contentUploaded.entryType -eq "NOTE"){
-            Write-Output "No submissions found"
+            if($contentUploaded.entryType -eq "NOTE"){
+                Write-Output "No submissions found"
+            } elseif($contentUploaded.mediaStatus -eq "ERROR") {
+                Write-Output "ERROR"
+            } else {
+                $ScriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+                $output = $ScriptPath + "/" + $content.fileName
+                $stream = $baseUrl + "/rest/timelines/stream/" + $id
+
+                $start_time = Get-Date
+
+                # Using System.Net.WebClient Speed is great as the HTTP response stream is buffered to disk throughout the download process.
+                # There is also the option of System.Net.WebClient.DownloadFileAsync(). If you'd like your script to continue while the file downloads in parallel.
+                $wc = New-Object System.Net.WebClient
+                $wc.Headers["Authorization"] = "Basic $BasicCreds"
+                try {
+                    $wc.DownloadFile($stream, $output)
+                    Write-Output "------------------------------------"
+                    Write-Output "|  Download completed successfully |"
+                    Write-Output "------------------------------------"
+                } catch [System.Net.WebException]  {  
+                    Write-Warning "Download of $stream failed"  
+                } finally {
+                    # once files are done downloading
+                    $wc.Dispose()
+                }
+                Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+            }
         }
-        else {
-            $ScriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
-            $output = $ScriptPath + "/" + $content.fileName
-            $stream = $baseUrl + "/rest/timelines/stream/" + $id
-
-            $start_time = Get-Date
-
-            # Using System.Net.WebClient Speed is great as the HTTP response stream is buffered to disk throughout the download process.
-            # There is also the option of System.Net.WebClient.DownloadFileAsync(). If you'd like your script to continue while the file downloads in parallel.
-            $wc = New-Object System.Net.WebClient
-            $wc.Headers["Authorization"] = "Basic $BasicCreds"
-            Register-ObjectEvent -InputObject $wc -EventName DownloadFileCompleted -Action { Write-Host 'Download completed successfully' }
-            $wc.DownloadFile($stream, $output)
-            Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-        }
-    }
-    else {
+    } catch {
         Write-Output "Error ocurred"
+        $err = $_.Exception #or try '$_' only
+        $StatusCode = $_.Exception.Response.StatusCode.value__
+        # $err | Get-Member
+        $err.Message
+        $StatusCode
+        Write-Output "==================================================================="
     }
 }
 else {
     Write-Output "Please provide Credentials"
 }
-
